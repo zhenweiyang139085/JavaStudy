bcfc1af1eeef809b9864995430ce54f5c0129947
yzw <869631950@qq.com>
2020-08-01 13:33:23 +0800

    工程创建，第一次添加文件

---
+++ DesignPattern/src/main/java/com/yzw/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.yzw.git;
+
+/**
+ * @Auther: yzw
+ * @Date: 2020/8/1 - 08 - 01 - 13:24
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}

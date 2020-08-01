e8ad80e90435a49a85efe398a1c5b99cb8ab0dce
yzw <869631950@qq.com>
2020-08-01 13:42:48 +0800

    创建分支Dev

--- DesignPattern/src/main/java/com/yzw/git/HelloGit.java
+++ DesignPattern/src/main/java/com/yzw/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("更新");
+        System.out.println("创建分支Dev");
     }
 }
